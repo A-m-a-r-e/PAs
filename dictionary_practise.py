0ee7c74212e03dc45fe4e122d3736f9632148a03
A-m-a-r-e <amare@hackthehood.org>
2021-06-21 09:23:34 -0700

    dictionary

---
+++ dictionary_practise.py
@@ -0,0 +1,21 @@
+foods = {"chicken":1.59, "beef":1.99, "cheese":1.00, "milk":2.50}
+NBA_players = {"Lebron James": 23, "Kevin Durant": 25, "Stephen Curry": 30, "Damian Lillard": 0}
+
+chicken_price = foods["chicken"]
+print(chicken_price)
+
+beef_price = foods["beef"]
+print(beef_price)
+
+cheese_price = foods["cheese"]
+print(cheese_price)
+
+milk_price = foods["milk"]
+print(milk_price)
+
+james_num = NBA_players["Lebron James"]
+print(james_num)
+
+shoes = {"Jordan_13": 1, "Yeezy": 8 }
+
+
---
+++ list_practice.py
@@ -0,0 +1,16 @@
+empty_list = []
+
+city_list = ["Oakland", "Atlanta", "New York City", "Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+print(city_list)
+city_list[0] = "San Francisco"
+city_list[2] = "Brooklyn"
+city_list[7] = "Hollywood"
+city_list[5] = "Tampa"
+print(city_list)
+
+sports_list = ["Basketball", "Soccer", "Football", "Hockey", "Track and Field", "Baseball", "Tennis",]
+print(sports_list)
+print(sports_list[5])
+print(sports_list[0])
+
+
